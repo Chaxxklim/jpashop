c0dfa04c746925c6af7ac7d4f29f7f087517144e
Chaxxklim <87353327+Chaxxklim@users.noreply.github.com>
2021-11-13 19:41:59 +0900

    null 사랑하겠어~ㅜㅜ

---
+++ src/main/java/jpabook/jpashop/domain/ex.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.domain;
+
+import java.util.Scanner;
+
+public class ex {
+    public static void main(String[] args) {
+    int hour;
+    int minute;
+    int second;
+
+    int tempHour;
+    int tempMinute;
+    int tempSecond;
+    int tempTime;
+    Scanner sc = new Scanner(System.in);
+    tempTime = sc.nextInt();
+
+
+    }
+}
